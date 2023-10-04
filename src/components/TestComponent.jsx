cb1f6fe42c48bb6d808f08007836d4b15e2e01b8
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2023-10-04 17:56:13 -0300

    First commit

---
+++ src/components/TestComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TestComponent() {
+  return (
+    <div>
+      <h2>Test Component</h2>
+      <p>Este es un componente de prueba.</p>
+    </div>
+  );
+}
+
+export default TestComponent;
