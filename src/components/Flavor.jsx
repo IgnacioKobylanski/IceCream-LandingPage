782aa70c4b962554773a1ba5e08700a8879be364
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2024-01-02 15:54:30 -0300

    minor changes in flavor.jsx

--- src/components/Flavor.jsx
+++ src/components/Flavor.jsx
@@ -2,6 +2,10 @@ import React from "react";
 
 function Flavor() {
     return (
-        <div className="flavors-main"></div>
+        <div className="flavors-main">
+            <table className="flavors-table"></table>
+        </div>
     );
 };
+
+export default Flavor;
