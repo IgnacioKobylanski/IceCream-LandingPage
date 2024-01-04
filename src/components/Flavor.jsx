1feacc19f6f1078890d410594dda5e46111f73cc
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2024-01-04 10:19:13 -0300

    somee lines from Flavor removed

--- src/components/Flavor.jsx
+++ src/components/Flavor.jsx
@@ -3,8 +3,7 @@ import React from "react";
 function Flavor() {
     return (
         <div className="flavors-main">
-            <table className="flavors-table"></table>
-            <section className="flavor-section"></section>
+
         </div>
     );
 };
