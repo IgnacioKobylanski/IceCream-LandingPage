cb5e19d4dfa3c6d73e4ee76f9e9546180818ec9d
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2024-01-05 19:12:49 -0300

    changes in Flavor component

--- src/components/Flavor.jsx
+++ src/components/Flavor.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Flavor() {
     return (
         <div className="flavors-main">
-
+            <section className="flavors-sections"></section>
         </div>
     );
 };
