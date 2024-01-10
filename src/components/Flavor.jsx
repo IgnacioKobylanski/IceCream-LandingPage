a317fb4afa87e04d39b93c5c302cafc71d43cfde
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2024-01-09 21:12:04 -0300

    minor changes

--- src/components/Flavor.jsx
+++ src/components/Flavor.jsx
@@ -4,7 +4,7 @@ function Flavor() {
     return (
         <div className="flavors-main">
             <section className="flavors-sections">
-                <h2 className="flavors-title"></h2>
+                <h2 className="flavors-title">Try our card!</h2>
             </section>
         </div>
     );
