106a3dc79ae3f62a40414430d8d9facb7ae45257
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2024-01-11 12:08:57 -0300

    some colors have been changed

--- src/components/Flavor.jsx
+++ src/components/Flavor.jsx
@@ -4,7 +4,7 @@ function Flavor() {
     return (
         <div className="flavors-main">
             <section className="flavors-sections">
-            
+            <div className="flavor-container"></div>
             </section>
         </div>
     );
