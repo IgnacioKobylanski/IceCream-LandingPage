d3259e5621a68c40cd675c6e4d37be481ab2b54b
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2024-02-07 19:03:24 -0300

    minor changes in flavor.jsx

--- src/components/Flavor.jsx
+++ src/components/Flavor.jsx
@@ -5,8 +5,9 @@ function Flavor() {
         <div className="flavors-main">
             <section className="flavors-sections">
                 <h1>Taste the Frozen Rainbow</h1>
-                <img src="" alt="" className="flavorimg" />
+                <img src="" alt="" className="flavorImg" />
                 <p className="description"></p>
+                <div className="vegan-flavors"></div>
             </section>
         </div>
     );
